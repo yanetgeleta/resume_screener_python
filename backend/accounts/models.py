afc31e2bc857331f09f92377818f1bd27b5a1df5
Yanet Geleta <yanetgele@gmail.com>
2026-08-14 12:22:24 +0300

    first commit, setup company model

---
+++ backend/accounts/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+
+# Create your models here.
+
+class CompanyManager(BaseUserManager):
+  """Company manager where email is used as the user name"""
+
+  def create_company(self, email, password=None, **extra_fields):
+    if not email:
+      raise ValueError("The Email filed must be set")
+    email = self.normalize_email(email)
+    user = self.model(email=email, **extra_fields)
+
+    user.set_password(password)
+    user.save(using=self._db)
+    return user
+  
+  def create_superuser(self, email, password=None, **extra_fields):
+    extra_fields.setdefault('is_staff', True)
+    extra_fields.setdefault('is_superuser', True)
+    extra_fields.setdefault('is_active', True)
+
+    if extra_fields.get('is_staff') is not True:
+      raise ValueError("Superuser must be staff")
+    if extra_fields.get('is_superuser') is not True:
+      raise ValueError("Super user must have super user true")
+    
+    return self.create_company(email, password, **extra_fields)
+  
+class Company(AbstractBaseUser, PermissionsMixin):
+  email = models.EmailField(unique=True, null=False, blank=False)
+  company_name = models.CharField(max_length=200)
+  is_active = models.BooleanField(default=True)
+  is_staff = models.BooleanField(default=False)
+
+  objects = CompanyManager()
+  USERNAME_FIELD = 'email'
+  REQUIRED_FIELDS = ['company_name']
+
+  def __str__(self):
+    return self.email
