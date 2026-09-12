e9c129a6178384beb3289ed40466be1749cf5607
yanetgeleta <yanetgele@gmail.com>
2026-09-12 23:09:46 +0300

    Groq api key solution at groq client instanciation

--- backend/jobs/groq_client.py
+++ backend/jobs/groq_client.py
@@ -1,11 +1,16 @@
 from django.conf import settings
 from groq import AsyncGroq, Groq
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
 
 groq_client_instance = Groq(
-    api_key=getattr(settings, "GROQ_API_KEY", "mock-key-for-tests")
+    api_key=os.getenv("GROQ_API_KEY", "mock-key-for-tests")
 )
 
 async_groq_client_instance = AsyncGroq(
-    api_key=getattr(settings, "GROQ_API_KEY", "mock-key-for-tests")
+    api_key=os.getenv("GROQ_API_KEY", "mock-key-for-tests")
 )
 
